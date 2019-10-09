d831dd1ea901b267aa56dd8a1481ce8c1c8c8023
Tevin Sherman <tevinsherman@yahoo.com>
2019-10-09 19:22:54 -0400

    add valid check

--- lib/wiz_app/cli.rb
+++ lib/wiz_app/cli.rb
@@ -2,24 +2,36 @@ class WizApp::CLI
 
   def call
     #welcome
-    puts "Welcome To The Washington Wizards App"
-    # get_roster
-    # get_season
-    #
+    puts "\nWelcome To The Washington Wizards App\n"
+    get_player
+    user_player
+    get_user_player
   end
 
-  def welcome
-    puts "Welcome To The Washington Wizards App"
-  end
-
-  def player
-    # To be scrape
-    @players = ["John Wall, Bradley Beal, Jeff Green"]
-  end
+  
 
   def get_player
+    # To be scrape
+    @players = ["John Wall", "Bradley Beal", "Jeff Green", "Thomas Byrant"]
+  end
+
+  def user_player
     #list players
-    @players = [""].each_with_index{||}
+    puts "Choose a Wizards player you would like to see"
+    @players.each_with_index do  |player, index| 
+      puts "#{index}. #{player}"
+    end
+  end
+
+  def get_user_player
+    chosen_player = gets.strip
+    binding.pry
+   # valid_input(chosen_player.to_i, @players)
+  #end
+  end
+
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0
   end
 
 
