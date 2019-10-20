5250b44cf95f97dada323f0464bd3602834843a1
Tevin Sherman <tevinsherman@yahoo.com>
2019-10-20 19:56:11 -0400

    refactoring

--- lib/wiz_app/cli.rb
+++ lib/wiz_app/cli.rb
@@ -5,12 +5,13 @@ class WizApp::CLI
   
   def call
     #welcome
-    puts "\nWelcome To The Washington Wizards App\n"
+    
+    puts
     @players = get_players
-    binding.pry
+    #binding.pry
     user_player
     get_user_player
-    exit
+    #end_program?#exit
   end
 
   
@@ -24,27 +25,48 @@ class WizApp::CLI
   end
 
   def user_player
+    #menu
     #list players
+    puts "\nWelcome To The Washington Wizards App\n"
+    puts
     puts "Choose a number to pick a Wizards player you would like to see."
-    min_num = 1
-    max_num = @players.length
+    puts
     #binding.pry
     @players.each_with_index do  |wizards, index| 
       puts "#{index+1}. #{wizards.name}"
     end
-  end
+    puts 
+    puts "Enter X to exit"
+   # input = gets.strip
+end
+
 
   def get_user_player
-    chosen_player = gets.strip.to_i
+    #menu
+    #min_num = 1
+    #max_num = @players.length
+    input = gets.strip
+
+    if input == "X" || input == "x"
+      end_program
+    end
+
+    chosen_player = input.to_i
     show_players_for(chosen_player) if valid_input(chosen_player, @players)
+      if !valid_input(chosen_player, @players)
+        puts "#{input} is invalid please enter a valid number between 
+        1 and  #{@players.length}" 
+        get_user_player
+      end
   end
 
   #def menu
     #puts "Enter"
   #end
 
-  def valid_input(input, data)
-    input.to_i <= data.length && input.to_i > 0
+  def valid_input(input, data) #should return true or false
+
+     input.to_i > 0 && input.to_i <= data.length
   end
 
 def show_players_for(chosen_player)
@@ -53,25 +75,26 @@ def show_players_for(chosen_player)
   #puts "They played in #{wizard.games}"
   puts "G:#{wizard.games} PTS:#{wizard.pts} FG:#{wizard.fg} FG_Percent:#{wizard.fg_percent}  3PT:#{wizard.threepts} FT:#{wizard.ft} Oreb:#{wizard.oreb}
   Dreb:#{wizard.dreb} Reb:#{wizard.reb} Ast:#{wizard.ast} Stl:#{wizard.stl} TO:#{wizard.turnovers} PF:#{wizard.pf}"
+  return_menu
 end
 
-def exit
+def return_menu
+  puts "Enter wiz to see other player stats or enter X to end"
+  input = gets.strip
+  if input == "x" || input == "X"
+    end_program
+  elsif input == "wiz"
+    user_player
+  else
+      puts "#{input} is invalid"
+      return_menu
+    end
+
+  end
+
+def end_program
   puts "Thanks for using the Wizard App"
   !exit
 end
 
 end
-
-
-
-#puts "Which team would you like to know more about? (Enter: 1-25 or exit)"
-	#	input = gets.strip.downcase
-			
-#		if input.to_i > 0
-#			team_menu(input)
-#		elsif input == "exit"
-#			goodbye	
-#		else
-#			puts "Try again!"
-#		end
-#	end
