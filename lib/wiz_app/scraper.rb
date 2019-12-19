db02a4830cf61b8e83fb727c65b933158c9fb3df
Tevin Sherman <tevinsherman@yahoo.com>
2019-12-19 18:25:15 -0500

    refactor scraper class

--- lib/wiz_app/scraper.rb
+++ lib/wiz_app/scraper.rb
@@ -24,30 +24,3 @@ class WizApp::Scraper
     end
   end
 end
-
-
-
-
-  
-#   def player_names
-    # player_name_cells = stat_page.css("td.player_name span.playerName a")
-    
-#     player_name_cells.map { |cell| cell.children.first.text }
-#   end
-
-#   def stat_page
-#     @stat_page ||= Nokogiri::HTML(open("https://www.nba.com/wizards/stats?season=2019-20&season_type=regular%20season"))
-#   end
-  
-    #def player_rows
-     # stat_page.css("table.stats-table.player-stats.season-averages tbody tr")
-    #end
-
-   #def player_stats
-    # doc = Nokogiri::HTML(open("https://www.nba.com/wizards/stats?season=2019-20&season_type=preseason"))
-  #    player_stats_num = doc.css("")
-
-  #    player_stats_num
-  #  end
-    
- #end
